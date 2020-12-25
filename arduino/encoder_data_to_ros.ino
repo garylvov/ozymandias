983a550785650b976c41719cd49c70e3879c7cc2
garylvov <67614381+garylvov@users.noreply.github.com>
2020-12-24 22:20:57 -0500

    Update encoder_data_to_ros.ino

--- arduino/encoder_data_to_ros.ino
+++ arduino/encoder_data_to_ros.ino
@@ -12,8 +12,8 @@ ros::NodeHandle nh;
 std_msgs::Int32 ros_counter_left;
 std_msgs::Int32 ros_counter_right;
 
-ros::Publisher left("left", &ros_counter_left);
-ros::Publisher right("right", &ros_counter_right);
+ros::Publisher left_encoder("left_encoder", &ros_counter_left);
+ros::Publisher right_encoder("right_encoder", &ros_counter_right);
 
 volatile int counter_left = 0;
 volatile int counter_right = 0;
@@ -28,15 +28,15 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(18), update_right, CHANGE);
 
   nh.initNode();
-  nh.advertise(left);
-  nh.advertise(right);
+  nh.advertise(left_encoder);
+  nh.advertise(right_encoder);
 }
 
 void loop() {
   ros_counter_left.data = counter_left;
   ros_counter_right.data = counter_right;
-  left.publish(&ros_counter_left);
-  right.publish(&ros_counter_right);
+  left_encoder.publish(&ros_counter_left);
+  right_encoder.publish(&ros_counter_right);
   nh.spinOnce();
 }
 
