ae346d7406ba92666dce6befe786743abfa06a63
garylvov <67614381+garylvov@users.noreply.github.com>
2021-01-02 12:27:03 -0500

    Update encoder_handler.py

--- ozymandias_pi/src/encoder_handler.py
+++ ozymandias_pi/src/encoder_handler.py
@@ -2,7 +2,7 @@
 import rospy
 import threading
 from sensor_msgs.msg import JointState
-from std_msgs.msg import Int32
+from std_msgs.msg import Float32, Int32
 
 left_encoder_pulses = 0
 right_encoder_pulses = 0
@@ -18,22 +18,38 @@ def right_callback(msg):
     global right_encoder_pulses
     right_encoder_pulses = msg.data
 
-def pulses_to_velocity(name, pulses):
-    vel_pub = rospy.Publisher(name, Int32, queue_size = 10)
-    vel = Int32()
+def left_pulses_to_velocity():
+    vel_pub = rospy.Publisher('left_vel', Float32, queue_size = 10)
+    vel = Float32()
     previous_time = 0
-    previous_pulses = pulses
+    previous_pulses = left_encoder_pulses
 
     while not (rospy.is_shutdown()):
-        now = rospy.Time.now()
-        if(now - previous_time >= 1):
+        now = rospy.Time.now().to_sec()
+        if(now - previous_time >= .25):
             # velocity = Δ displacement / Δ time
-            vel.data = (pulses - previous_pulses) / (now - previous_time)
+            vel.data = (left_encoder_pulses - previous_pulses) / (now - previous_time)
             vel.data *= conversion_factor
             previous_time = now
-            previous_pulses = pulses
-    vel_pub.publish(vel)
+            previous_pulses = left_encoder_pulses
+        vel_pub.publish(vel)
+    
+def right_pulses_to_velocity():
+    vel_pub = rospy.Publisher('right_vel', Float32, queue_size = 10)
+    vel = Float32()
+    previous_time = 0
+    previous_pulses = right_encoder_pulses
 
+    while not (rospy.is_shutdown()):
+        now = rospy.Time.now().to_nsec()
+        if(now - previous_time >= 1):
+            # velocity = Δ displacement / Δ time
+            vel.data = (right_encoder_pulses - previous_pulses) / (now - previous_time)
+            vel.data *= conversion_factor
+            previous_time = now
+            previous_pulses = right_encoder_pulses
+        vel_pub.publish(vel)
+    
 def pulses_to_JointState():
     states_pub = rospy.Publisher('joint_states', JointState, queue_size = 10)
     state = JointState()
@@ -52,8 +68,8 @@ if __name__=='__main__':
     jointstate = threading.Thread(target = pulses_to_JointState)
     jointstate.start()
 
-    left_vel = threading.Thread(target = pulses_to_velocity, kwargs = dict(name = 'left_vel', pulses = left_encoder_pulses))
+    left_vel = threading.Thread(target = left_pulses_to_velocity)
     left_vel.start()
 
-    right_vel = threading.Thread(target = pulses_to_velocity, kwargs = dict(name = 'right_vel', pulses = right_encoder_pulses))
+    right_vel = threading.Thread(target = right_pulses_to_velocity)
     right_vel.start()
